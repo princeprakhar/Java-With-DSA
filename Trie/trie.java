524f90371b5525c0a30a6d4e1a6fa7326f5032f8
princeprakhar <103184248+princeprakhar@users.noreply.github.com>
2024-03-28 00:17:03 +0530

    insert function added in trie

--- Trie/trie.java
+++ Trie/trie.java
@@ -1,18 +1,23 @@
 package Trie;
 
 public class trie {
-    private Node root;
+    private  Node root;
     public trie(){
         root = new Node();
-
     }
+    //defining the insertion for the String in Trie.
     public void insert(String word)
     {
         Node node = root;
         for(int i=0;i<word.length();i++)
         {
-
+            if(!node.containsKey(word.charAt(i)))
+            {
+                node.put(word.charAt(i),new Node());
+            }
+            node.getNode(word.charAt(i))
         }
+        node.endOfWord = true;
     }
 
 }
@@ -25,11 +30,18 @@ class Node {
         alphabet = new Node[26];
         endOfWord = false;
     }
+    //contains method
+    boolean containsKey(char ch)
+    {
+        return (!(alphabet[ch - 'a'] == null));
+    }
+    //defining the put method
     public void put(char ch, Node node)
     {
          alphabet[ch - 'a'] = node;
 
     }
+    //defining the get method for the node.
     public Node getNode(char ch)
     {
         return alphabet[ch - 'a'];
