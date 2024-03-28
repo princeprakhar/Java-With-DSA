80ff2cdffd7f379ee7b451591eb95343b62eb3e1
princeprakhar <103184248+princeprakhar@users.noreply.github.com>
2024-03-28 10:01:42 +0530

    trie updated

--- Trie/trie.java
+++ Trie/trie.java
@@ -1,7 +1,9 @@
 package Trie;
 
+import java.util.Scanner;
+
 public class trie {
-    private  Node root;
+    private final Node root;
     public trie(){
         root = new Node();
     }
@@ -15,14 +17,63 @@ public class trie {
             {
                 node.put(word.charAt(i),new Node());
             }
-            node.getNode(word.charAt(i))
+            node.getNode(word.charAt(i));
         }
         node.endOfWord = true;
     }
+//    defining the search method
+    public boolean search(String word){
+        Node node = root;
+        for(int i=0;i<word.length();i++)
+        {
+            if(!node.containsKey(word.charAt(i)))
+            {
+                return false;
+            }
+        }
+        return node.endOfWord;
+    }
+    //defining the stardWithWord method
+    public boolean startWithWord(String word)
+    {
+        for(int i=0;i<word.length();i++)
+        {
+            if(!root.containsKey(word.charAt(i)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    //main method
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of the String's array" );
+        int n = sc.nextInt();
+        trie root = new trie();
+        String []s= new String[n];
+        System.out.println("Enter String for the Array:");
+        for(int i=0;i<n;i++)
+        {
+            s[i] = sc.next();
+            root.insert(s[i]);
+        }
+        System.out.println("Enter the String you want to search:");
+        String stringToSearch = sc.next();
+        System.out.println(root.search(stringToSearch)?"The String is in the container": "The String is not present in the conatiner");
+        System.out.println("Enter the Prefix you want to search:");
+        String stringStartWith = sc.next();
+        System.out.println(root.startWithWord(stringStartWith)?"The String is in the container has Prefix": "The String with Prefix is not present in the conatiner");
+
+
+
+    }
 
 }
+
 class Node {
-    Node []alphabet;//Initialization for map for character all possible alphabets.(Assuming all the character are small case.
+    Node []alphabet;//Initialization for map for character all possible alphabets.(Assuming all the character are small case).
     boolean endOfWord;//flag which will indicate for the end for the words.
     //constructor
     public Node()
